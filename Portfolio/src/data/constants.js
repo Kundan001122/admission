f17f0de7fcf5e37564126b25c044686e45c2f676
Kundan Kumar Mandal <kundankumar6204340@gmail.com>
2024-09-30 02:37:20 +0530

    portfolio

---
+++ Portfolio/src/data/constants.js
@@ -0,0 +1,286 @@
+export const Bio = {
+    name: "Kundan Kumar",
+    roles: [
+        "Full Stack Developer",
+        ".Net Developer",
+        "Programmer",
+    ],
+    description: "I have completed bca From Aryabhatta Knowledge University Patna & My highest CGPA is 7.75 Along with my degree i have completed final year project my final year project is Blog Application & that project is under java Technology. I have completed my Full stack devlopment course (Mern stack) from Skill Academy. I have learn many Technology like : HTML , Css , Bootstrap , Javascript , Node.js Express.js , MongoDB ,  React & Tailwind css, with many few project created : 2. My_Personal Portfolio |Used Technology : HTML , CSS & BOOTSTRAP | 2.Music Application | Used Technology : Javascript | 3. To-Do Application | Used Technology : Javascript | 4. Linkedin Clone | Used Technology : HTML , CSS & BOOTSTRAP | Photography mini project | Used Technology : HTML , CSS & BOOTSTRAP | 5. Watch web Application | Used Technology : Javascript |& 6.Major project is: Weather Cheak Application | Used Technology : React Intern at || @Topperworld Basic Knowledge of Java programing | | C Language | | Python | | PHP & Currently I'm learning : C# programing Language | .Net | ",
+
+    github: "https://github.com/Kundan001122",
+    resume: "https://docs.google.com/document/d/1UdnglRLXAND3iu2SBlFfr5aUroYDFRZ8-XEjBF-mqTk/edit#heading=h.3znysh7",
+    linkedin: "https://www.linkedin.com/in/kundan-kumar-66b73b259/",
+    insta: "https://www.instagram.com/kk62043/",
+
+};
+
+export const skills = [{
+        title: "Frontend",
+        skills: [{
+                name: "React Js",
+                image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+            },
+            {
+                name: "Redux",
+                image: "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+            },
+            {
+                name: "HTML",
+                image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+            },
+            {
+                name: "CSS",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+            },
+            {
+                name: "JavaScript",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+            },
+            {
+                name: "Bootstrap",
+                image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            },
+        ],
+    },
+    {
+        title: "Backend",
+        skills: [{
+                name: "Node Js",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/120px-Node.js_logo.svg.png",
+            },
+            {
+                name: "Express Js",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/120px-Expressjs.png",
+            },
+            {
+                name: "MongoDB",
+                image: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/MongoDB_Fores-Green.svg/250px-MongoDB_Fores-Green.svg.png",
+            },
+            {
+                name: "Java",
+                image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/121px-Java_programming_language_logo.svg.png",
+            },
+            {
+                name: "PHP",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/121px-PHP-logo.svg.png",
+            },
+            {
+                name: "C# Programming",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/128px-C_Sharp_Logo_2023.svg.png",
+            },
+        ],
+    },
+    // {
+    //     title: "Video Editing",
+    //     skills : [
+    //       {
+    //         name: "Filmora"
+    //       }
+    //     ]
+    // },
+    {
+        title: "Others",
+        skills: [{
+                name: "Git",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/150px-Git-logo.svg.png",
+            },
+            {
+                name: "GitHub",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/200px-GitHub_Invertocat_Logo.svg.png",
+            },
+            {
+                name: "Netlify",
+                image: "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+            },
+            {
+                name: "VS Code",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+            },
+            {
+                name: "xampp server",
+                image: "https://upload.wikimedia.org/wikipedia/en/thumb/7/78/XAMPP_logo.svg/120px-XAMPP_logo.svg.png",
+            },
+        ],
+    },
+];
+
+export const experiences = [{
+        id: 0,
+        img: "https://firebasestorage.googleapis.com/v0/b/image-ac3b3.appspot.com/o/topperworld.jpg?alt=media&token=bb949f93-39b0-4111-9b9e-71a36334359c",
+        role: "Web Developer Internship",
+        company: "Topperworld",
+        date: "February 2024 - March 2024",
+        desc: "Working on three project , Portfolio , LinkedIn and Quara post Crud Application",
+        skills: [
+            "React",
+            "CSS",
+            "JS",
+            "MongoDB",
+            "Node JS",
+            "Express JS",
+            "Java",
+            "Php"
+        ],
+        doc: "",
+
+    },
+    // {
+    //     id: 1,
+    //     img: "https://firebasestorage.googleapis.com/v0/b/image-ac3b3.appspot.com/o/WordPress_blue_logo.svg.png?alt=media&token=0fc23496-7159-4bd0-be34-4d9208034de9",
+    //     role: "React Developer",
+    //     company: "Freelanceing",
+    //     date: "January 2020",
+    //     desc: "Developing the website using wordpress",
+    //     skills: [
+    //         "Wordpress",
+    //         "HTML",
+    //         "CSS",
+    //     ],
+    // },
+    // {
+    //     id: 0,
+    //     img: "https://firebasestorage.googleapis.com/v0/b/image-ac3b3.appspot.com/o/testbook.png?alt=media&token=c18abc95-ce8c-48df-8d78-b594ac6c566d",
+    //     role: "Senior Education counselor",
+    //     company: "Testbook",
+    //     date: "May 2022 - May 2023",
+    //     desc: "Working As a Senior Councellor at ' Testbook ' , my role : 1.Call The Student And Tell them About The Course , 2.Provide the offer,3.Feedback ,4.Sale Course,5.Communication Skills,6.Counselling the student and give the best advice according to the students ",
+    //     skills: [
+    //         "Communication",
+    //         "English",
+    //         "Listening",
+    //         "Sale"
+    //     ],
+    //     doc: "",
+
+    // },
+    // {
+    //     id: 0,
+    //     img: "https://firebasestorage.googleapis.com/v0/b/image-ac3b3.appspot.com/o/the_good_network_logo.jpg?alt=media&token=7becb442-c61a-462b-9502-5f60de03806d",
+    //     role: "Corporate Communications Manager",
+    //     company: "Good Network",
+    //     date: "May 2023",
+    //     desc: "As a Communication Manager at 'Good Network ' my role : 1.Managing social media like Facebook, Instagram, YouTube ,Snapchat , 2.Uploading the video on social media account ,3.Keyword Research ,4.Deal With different companies ,5.Brand Deals ,6.Communication Skills ,7.Social Skills",
+    //     skills: [
+    //         "Communication",
+    //         "English",
+    //         "Listening",
+    //         "Managing Social Media Account",
+    //         "Video editing",
+    //         "Research"
+    //     ],
+    //     doc: "",
+
+    // },
+
+];
+
+export const education = [{
+        id: 0,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFz8jxWZ9Xb5rLmBoY3DglNa5btIhmX-MxlQ&usqp=CAU",
+        school: "Aptron Company Noida",
+        date: "December 2023 - June 2024",
+        grade: "A",
+        desc: "I joined the Full Stack development Programm in Dec 2023 , Here I learn HTML , CSS , JavaScript , React , Redux, Bootstrap , node JS , Express JS, MongoDB",
+        degree: "Full Stack Developer",
+    },
+    {
+        id: 1,
+        img: "https://edufever.in/colleges/wp-content/uploads/2022/06/MBIT-Forbesganj.jpg",
+
+        school: "Moti Babu Institute of Technology  , Araria , Forbesganj",
+        date: "July 2020 - December 2023",
+        grade: "75%",
+        desc: "I completed my BCA from Moti Babu Institute of Technology college ,Araria ,Forbesganj",
+        degree: "BCA",
+    },
+    {
+        id: 2,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Yadava_College%2C_Madurai.jpg/800px-Yadava_College%2C_Madurai.jpg",
+
+        school: "M.L.D.P.K Yadav college  , Araria , Bihar",
+        date: "July 2018 - December 2020",
+        grade: "60%",
+        desc: "I completed my 12th from M.L.D.P.K. Yadav College ,Araria ,Bihar",
+        degree: "Art's",
+    },
+    {
+        id: 3,
+        img: "https://files.yappe.in/place/full/madhya-vidyalya-paik-tola-7953190.webp",
+        school: "Madhya Vidyalya Paik Tola High School , Araria",
+        date: "April 2018 - March 2020",
+        grade: "%",
+        desc: "",
+        degree: "I have done my 10th from bihar Board.",
+    },
+
+];
+
+export const projects = [
+
+    {
+        id: 0,
+        title: "Todo Web App",
+        date: "January 2023",
+        description: "A todo List , I am using HTML , CSS and Javascript",
+        image: "https://firebasestorage.googleapis.com/v0/b/image-ac3b3.appspot.com/o/Todo.png?alt=media&token=d439d90b-2597-45e8-bfa8-1498f5f5ae21",
+        tags: ["HTML", "Netlify", "CSS", "Javascript"],
+        category: "web app",
+        github: "https://github.com/Kundan001122",
+        webapp: "https://todoapplicationkundan.netlify.app/",
+    },
+
+    {
+        id: 1,
+        title: "Music App",
+        date: "December 2023",
+        description: "This is music web app, user can next, previous, play etc. using javascript",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/The_Sounds_of_Earth_-_GPN-2000-001976.jpg/260px-The_Sounds_of_Earth_-_GPN-2000-001976.jpg",
+        tags: ["JavaScript", "VS Code", "HTML", "CSS"],
+        category: "web app",
+        github: "https://github.com/Kundan001122",
+        webapp: "https://musicplayerkundan.netlify.app/",
+    },
+    {
+        id: 2,
+        title: "LinkedIn",
+        date: "December 2023",
+        description: "This is LinkedIn web clone application, using HTML, Css, Javascript",
+        image: "https://lh3.googleusercontent.com/pw/AP1GczOgy4bMnEY5OE0Hbs5HZ3SRbHSms379Kf1TjXCoFp1miCSlx6JUDrpkG6C9DsyYmnlGr5Gy4V9Yz6gU9s5sH9qc-htJA3sfgtuF4GA3-eoHmsqHZp0UaQsuaMPLtblTF4H4p-zwmfdyF7csXkJM8fAhSve-qI-WFPWeWjxWS-lN11dtiSxY46O6Xu7r5aRJasqmmMblTD1UPp9dYYRSyvTcoRE-TVeImDYDvmozj_o8k5WY7wfPR7jKNqiBbLprNwgB2ibx9FZ22j2A8NxlYKQz5XBwtAjvbqAUkYqrEm9ype71iWSpkToza1S5vVFmTfDX88YlBEbGa3Deg_pHB-ONKkiaZP8lxTyKfOFTeasBHMmad7GO5tDobTMcc_RIzyiW-ogU0BrIK80s7Jak_iLOYJB-OOczvx3MGXja_hx6Vog4oMPHrf_0Jok-ZwgqGvbx4mEYV3T79Sz1G7b0tr7Li-abJVaUxIjblLCKJJcCniG_GjxJXQb0ur23ENDbiWKeucvKU7GSsBCJpwCkKZHVFdJ-fcnsjFm0ysNm1VxNuOauqIlApWwrAYOZbyscGjl8Ei64YAWOo24wJ3gG2aPhIiR3C-ZVTp1erRr9nryPTs5F_sdEduSNjIGN5r7MmP57CEkId2JopBGqHHwU65fvjp01WjLcOeEJPEOcW5fqd6TXzwZv72Wa5EochxTJD9x6FAtWyIJwCnrqIaZpHi2vnlTxLihPSKE2-kSubD7XwjK1W_C6sa_xtJYJgWvB-z65ALs-JosrNmGbeDRBLTi5Hgvse53vpH2wmLECwNTWY-btpEMJV9jLBJq4Qa4n6NXmHjA77rwcjytPLcRnUHCP8ohg0tca1Uu-p5iBqF6Uz7RsM2Cu1CZXynyKq7z4xeQ11g_sgzLtPoluojdOv8uoRfYQ1ew7omOJVkjyKIQFC85LnfzIddqSIa4=w1140-h641-s-no-gm?authuser=0",
+        tags: ["VS Code", "HTML", "CSS"],
+        category: "web app",
+        github: "https://github.com/Kundan001122",
+        webapp: "https://elaborate-cupcake-59cb63.netlify.app/",
+    },
+    {
+        id: 3,
+        title: "Photography",
+        date: "January 2024",
+        description: "This is Fully responsive Mobile photography web clone using HTML, Css, Media Query",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Photography_by_Victor_Albert_Grigas_%281919-2017%29_00232_Old_Town_Art_Fair_Chicago_1968_%2823705070328%29.jpg/240px-Photography_by_Victor_Albert_Grigas_%281919-2017%29_00232_Old_Town_Art_Fair_Chicago_1968_%2823705070328%29.jpg",
+        tags: ["VS Code", "HTML", "CSS", "Javascript"],
+        category: "web app",
+        github: "https://github.com/Kundan001122",
+        webapp: "https://photographywebsitekundan.netlify.app/",
+    },
+    {
+        id: 4,
+        title: "Portfolio Website",
+        date: "January 2024",
+        description: "i'm using technology HTML, Css, Javascript, Bootstrap",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Portfolio.hu_full_logo.png",
+        tags: ["VS Code", "HTML", "CSS", "Bootstrap"],
+        category: "web app",
+        github: "https://github.com/Kundan001122",
+        webapp: "https://kundanfullstackdevloper.netlify.app/",
+    },
+
+
+];
+
+// export const TimeLineData = [
+//     { year: 2019, text: "Started my journey" },
+//     { year: 2020, text: "Worked as a Wordpress freelance developer" },
+//     { year: 2022, text: "Founded HTML ,CSS , JavaScript Course" },
+//     { year: 2023, text: "Join Full Stack Development Programm" },
+//     { year: 2023, text: "Join Intership programm at Topperworld" }
+// ];
